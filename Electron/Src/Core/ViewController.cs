0b32c5c67368ea07bd19db6b4b4bf82d0b63e834
Adam Venturella <aventurella@blitzagency.com>
2016-02-05 06:34:10 -0800

    adds core ViewController

---
+++ Electron/Src/Core/ViewController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+namespace Electron
+{
+	public class ViewController<T>: MonoBehaviour where T: UnityEngine.Component{
+		public T View;
+
+		void Start(){
+			View = gameObject.GetComponents<T>()[0];
+			Debug.Log(String.Format("Found View: {0}", View));	
+			onStart();
+		}
+
+		void Update(){
+			onUpdate();
+		}
+
+		virtual public void onStart(){}
+		virtual public void onUpdate(){}
+
+		public void addChild(Component child, bool worldPositionStays = false){
+			child.transform.SetParent(View.transform, worldPositionStays);
+		}
+
+	}
+}
+
